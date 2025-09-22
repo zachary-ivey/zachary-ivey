c7ca97e3234d619dd301dc6f0a82dd4325a6d169
Zachary Ivey <zacharyi8283@student.faytechcc.edu>
2025-09-22 00:57:46 +0000

    pt5

--- M2T1.cpp
+++ M2T1.cpp
@@ -12,6 +12,7 @@
  
     string first_name, last_name, full_name; //holds customer name
     string product = "apples"; //change to whatever you like
+    int amount_purchased;
     double cost_each = 0.99;
     double total_cost;
 
@@ -26,12 +27,17 @@
     full_name = first_name + " " + last_name;
     cout << "Nice to meet you, " << full_name << endl;
 
-
     //Ask how much they'd like to purchase
+    cout << "How many " << product << " would you like today? ";
+    cin >> amount_purchased;
+
 
     //Calculate total price
+    total_cost = amount_purchased * cost_each;
     //Give the result
-
+    cout << "For " << amount_purchased << " " << product << endl;
+    cout << "That will be: $" << total_cost << endl;
+    cout << "Thank you for shopping with us!" << endl;
 
 
     return 0; //no errors
