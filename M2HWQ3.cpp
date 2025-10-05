8c2b62be203f0d02557d5ed02fcf3b7835f343d0
Zachary Ivey <zacharyi8283@student.faytechcc.edu>
2025-10-05 16:21:48 +0000

    M2HWQ3

---
+++ M2HWQ3.cpp
@@ -0,0 +1,28 @@
+//CSC 134
+//M2HW - SILVER
+//Zachary Ivey
+//10/5/2025
+
+// ftcc_cheer.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Rcreating the variables
+    string letsGo = "Let's go ";
+    string school = "FTCC";
+    string team = "Trojans";
+
+    // combines strings
+    string cheerOne = letsGo + school;
+    string cheerTwo = letsGo + team;
+
+    // Displays cheers
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
