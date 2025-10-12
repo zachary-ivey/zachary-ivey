5206fa9be7d98f19883b33bb1170339d8db6c755
Zachary Ivey <zacharyi8283@student.faytechcc.edu>
2025-10-12 02:49:13 +0000

    M3LAB2.1

--- M3LAB2.cpp
+++ M3LAB2.cpp
@@ -2,4 +2,21 @@
 //M3LAB2
 //Zachary Ivey
 //10/10/2025
-//Program will give a letter grade based on
+//Program will give a letter grade based on the input
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    double grade;
+
+    cout << "Enter your current grade: ";
+    cin >> grade;
+    //asks the user for their grade
+
+    char lettergrade;
+    
+    if (grade >= 90 && grade <= 100)
+    lettergrade = "A";
+    
+}
