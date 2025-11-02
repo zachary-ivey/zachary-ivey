169b08cba76b4bc31c60daea720d61fc7ac8cc26
Zachary Ivey <zacharyi8283@student.faytechcc.edu>
2025-11-02 21:33:05 +0000

    M4HW1.S

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -5,12 +5,16 @@
 #include <iostream> 
 using namespace std; 
 int main() { 
-    int firstNum, secondNum, answer;
-     firstNum = 5; 
-     //secondNum = 1; 
-     //answer = firstNum + secondNum; 
-     for (int i=1; i<= 12; i++) { 
-        answer = firstNum * i; 
-        cout << firstNum << " times " << i << " is " << answer << endl; 
-    } 
+    
+    int firstNum, answer;
+
+    cout << "Enter a number from 1 to 12: ";
+    cin >> firstNum;
+
+    for (int i = 1; i <= 12; i++) {
+        answer = firstNum * i;
+        cout << firstNum << " times " << i << " is " << answer << endl;
+    }
+
+    return 0;
 }
